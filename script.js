f30d34bae86c61b4acaaeb9188b6664bd0fd4c70
LonelyCigarette <fftoken183@gmail.com>
2025-01-18 19:49:23 +0530

    tabbed componet is added

--- script.js
+++ script.js
@@ -54,3 +54,32 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+///////////////////////////////////////
+// Tabbed component
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  //make sure we click btn element even if click in span which is in btn.
+  const clicked = e.target.closest('.operations__tab');
+  
+  // Guard clause
+  //we can't use if (e.target.classList.contains('btn')) 
+  //because if we press in span element then it won't work
+  if (!clicked) return;
+  
+    // Remove active classes
+    tabs.forEach(t => t.classList.remove('operations__tab--active'));
+    tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  
+    // Activate tab
+    clicked.classList.add('operations__tab--active');
+  
+    // Activate content area
+    document
+      .querySelector(`.operations__content--${clicked.dataset.tab}`)
+      .classList.add('operations__content--active');
+});
